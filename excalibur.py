02ebfd6c27321f93200677661ad6677c2444f731
david <davidnagler24@gmail.com>
2024-03-11 09:37:57 +0100

    starting over with cx_freeze

--- excalibur.py
+++ excalibur.py
@@ -1,18 +1,5 @@
-import os
-import subprocess
-import sys
-
-# checks whether the platform used is windows, for using applicable shell command to activate venv
-is_windows = sys.platform.startswith('win')
-
-if is_windows:
-    subprocess.Popen(os.path.join("venv", "Scripts", "activate"), shell=True)
-else:
-    subprocess.Popen(os.path.join("venv", "bin", "activate"), shell=True)
-
-
 import pyautogui
 
-print(pyautogui.displayMousePosition())
 
+pyautogui.displayMousePosition()
 
